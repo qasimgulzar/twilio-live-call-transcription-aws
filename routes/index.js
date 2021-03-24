a69360c32549b8d659cdf0304e5d3813eda1f221
qasimgulzar <pent007#@>
2021-03-24 15:41:21 +0500

    [INIT] twilio call stream transcription example.

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post("/", (req, res) => {
+  res.set("Content-Type", "text/xml");
+
+  res.send(`
+    <Response>
+      <Start>
+        <Stream url="wss://${req.headers.host}/"/>
+      </Start>
+      <Say>I will stream the next 60 seconds of audio through your websocket</Say>
+      <Pause length="60" />
+    </Response>
+  `);
+});
+
+module.exports = router;
